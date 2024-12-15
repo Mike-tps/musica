905c126a90c195775b5753d26e039403288c1235
mike_tps <miguel.cabrera.landin@gmail.com>
2024-12-15 15:52:55 +0100

    feat: image hover sound

--- musica.js
+++ musica.js
@@ -1,6 +1,23 @@
 let lastStarTime = 0; // Tiempo de la última estrella
 const starInterval = 100; // Intervalo en milisegundos para crear estrellas
 
+// Importamos las imagenes y el sonido
+const images = document.querySelectorAll('.album');
+const sound = document.getElementById('wii');
+
+// Añadir un listener a cada imagen
+images.forEach(image => {
+    image.addEventListener('mouseover', () => {
+        sound.currentTime = 0; // Reset sound to start
+        sound.play(); // Play sound
+    });
+
+    image.addEventListener('mouseout', () => {
+        sound.pause(); // Pause sound when mouse leaves
+    });
+});
+
+// Crear una estrella en el mouse
 document.addEventListener('mousemove', function(e) {
     const currentTime = Date.now();
     if (currentTime - lastStarTime > starInterval) {
@@ -9,6 +26,7 @@ document.addEventListener('mousemove', function(e) {
     }
 });
 
+// Crear una estrella
 function crearEstrella(x, y) {
     const estrella = document.createElement('img');
     estrella.src = 'media/img/star.png'; // Cambia esto por la ruta de tu imagen
